f406601450c23536696bf140c1757ef6471d0233
Dominic Ferrando <dominicf2001@gmail.com>
2024-12-16 21:17:26 -0500

    setting up ofx conversion logic

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import json
+from ofxtools.header import make_header
 import plaid
 from plaid.api import plaid_api
 from plaid.model.transactions_sync_request import TransactionsSyncRequest
 from plaid.model.accounts_get_request import AccountsGetRequest 
+from ofxtools.models import *
+from ofxtools.utils import UTC  
+import xml.etree.ElementTree as ET
+from datetime import datetime
+from decimal import Decimal
 
 def get_transactions():
     response = client.transactions_sync(
@@ -31,6 +37,28 @@ def get_accounts():
     )
     return response["accounts"]
 
+def construct_ofx(transactions):
+    # for account_id in transactions.keys():
+    #     account_transactions = transactions[account_id]
+
+    ledgerbal = LEDGERBAL(balamt=Decimal("150.65"), dtasof=datetime(2024, 1, 1, tzinfo=UTC))
+    bankacctfrom = BANKACCTFROM(bankid="1234", acctid="4321", accttype="CHECKING")
+    stmtrs = STMTRS(curdef="USD", bankacctfrom=bankacctfrom, ledgerbal=ledgerbal)
+
+    # wrappers
+    status = STATUS(code = 0, severity="INFO")
+    stmttrnrs = STMTTRNRS(trnuid="0", status=status, stmtrs=stmtrs)
+    bankmsgsrs = BANKMSGSRSV1(stmttrnrs)
+    sonrs = SONRS(status=status, dtserver=datetime.now(UTC), language="ENG")
+    signonmsgs = SIGNONMSGSRSV1(sonrs=sonrs)
+
+    # finish 
+    ofx_root = OFX(signonmsgsrsv1=signonmsgs, bankmsgsrsv1=bankmsgsrs).to_etree()
+    message = ET.tostring(ofx_root).decode()
+    header = str(make_header(version=220))
+    return header + message
+    
+
 def main():
     global client
     global secrets
@@ -49,9 +77,7 @@ def main():
     api_client = plaid.ApiClient(configuration)
     client = plaid_api.PlaidApi(api_client)
 
-    transactions = get_transactions()
-    print(transactions)
-
+    print(convert_transactions_to_ofx("test"))
 
 if __name__ == "__main__":
     main()
